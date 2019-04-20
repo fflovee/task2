d430a265dd2abc2e6fb8a3c94cbad3b87230389e
fflovee <592005470@qq.com>
2019-04-20 22:49:21 +0800

    修改

--- task2/src/main/java/com/jnshu/mapper/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.jnshu.mapper;
-
-import com.jnshu.pojo.Student;
-
-public interface StudentDao {
-
-    Student selectStudent(String name);
-
-}
--- task2/src/main/java/com/jnshu/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.jnshu.service;
-
-import com.jnshu.pojo.Student;
-
-public interface StudentService {
-
-    Student selStuByName(String name);
-
-}
